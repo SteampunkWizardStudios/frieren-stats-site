219420c8aa5c2ecc77cd6fe5247edffae0dbebd9
TheOreoTM <qwertzybad@gmail.com>
2024-12-22 08:51:00 +0500

    fix: Bandage fix for the hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" data-lt-installed="true">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
