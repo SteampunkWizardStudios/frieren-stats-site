1f8426114d9f6148fd7bfafa88ae23f1ad2a500c
steampunkWizard <elijahanelson2nd@gmail.com>
2024-12-29 07:40:33 -0500

    remove rank page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,12 +35,6 @@ export default function Home() {
           >
             Leaderboard 2 Page
           </Link>
-          <Link
-            href="/rank"
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-          >
-            Rank Page
-          </Link>
           <SignIn />
         </div>
       </main>
