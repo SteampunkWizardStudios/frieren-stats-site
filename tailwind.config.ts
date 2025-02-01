1f36f348accd121689db5e11382fd1d5bd8f306b
steampunkWizard <elijahanelson2nd@gmail.com>
2025-02-01 18:15:25 -0500

    add bg-frieren-green

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,9 +53,6 @@ const config: Config = {
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
         },
-		frieren: {
-			green: "linear-gradient(0deg, rgba(var(--color-blue),1) 0%, rgba(var(--color-green),1) 100%)"
-		}
       },
       borderRadius: {
         lg: "var(--radius)",
