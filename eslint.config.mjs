5991aab9d24c8372fa1cec85d5a9afed8061ee60
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-23 10:48:57 -0400

    fix eslint for realsies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,34 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
 });
 
-export default defineConfig([{
-    extends: compat.extends("next/core-web-vitals", "next/typescript"),
-
+const eslintConfig = [
+  {
+    ignores: [".next"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
-        "@typescript-eslint/no-unused-vars": ["off", {
-            argsIgnorePattern: "^_",
-        }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          args: "all",
+          vars: "all",
+          ignoreRestSiblings: false,
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "off",
     },
-}]);
+  },
+];
+
+export default eslintConfig;
