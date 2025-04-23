27e76d789b68853b8696c5024c1b53ef14bac3c9
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-23 17:03:20 -0400

    add middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { auth as middleware } from "@/lib/auth";
 
+export const config = { matcher: ["/rank", "/list"] }
+
 // Middleware to keep the session alive, this will update the session expiry every time its called.
