e788364457a1857099ce08bee13d949508b81a45
steampunkWizard <elijahanelson2nd@gmail.com>
2025-04-23 11:20:47 -0400

    housekeeping + update deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { auth } from "@/lib/auth";
 import Image from "next/image";
 import Link from "next/link";
 
-const session = await auth();
-const username = session?.user?.username ?? "Sign in first";
-const displayName = session?.user?.name ?? "Sign in first";
-const userId = session?.user?.id ?? "None";
+export default async function Home() {
+  const session = await auth();
+  const username = session?.user?.username ?? "Sign in first";
+  const displayName = session?.user?.name ?? "Sign in first";
+  const userId = session?.user?.id ?? "None";
 
-export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
